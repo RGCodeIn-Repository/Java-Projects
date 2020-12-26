46e52cfaab9159d025ab3a26cb2efb8df0d0f11d
Rambabu Verma <ram.verma@yash.com>
2020-12-26 18:27:42 +0530

    Added initial code for core java main class stating point

---
+++ src/com/core/practice/MainTest.java
@@ -0,0 +1,13 @@
+package com.core.practice;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Guys... this is java core starting point");
+		for(int i =1 ; i<=100; i++){
+			System.out.println(i+",");
+		}
+	}
+
+}
